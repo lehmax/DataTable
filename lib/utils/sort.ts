15505113ac18cb9ba787d8682a8dd084affa35e4
Maxime Leherquer <45520008+lehmax@users.noreply.github.com>
2024-08-19 13:41:02 +0200

    chore: Update sorting logic in sortCollection function

--- lib/utils/sort.ts
+++ lib/utils/sort.ts
@@ -11,14 +11,19 @@ export const sortCollection = (
 
     if (!valueA && !valueB) return 0;
 
+    // Order by date
+    if (isValidDate(valueA) && isValidDate(valueB)) {
+      return order === "ascending"
+        ? new Date(valueA).getTime() - new Date(valueB).getTime()
+        : new Date(valueB).getTime() - new Date(valueA).getTime();
+    }
+
     return order === "ascending"
-      ? valueA.localeCompare(valueB, "en", {
-          numeric: true,
-          sensitivity: "base",
-        })
-      : valueB.localeCompare(valueA, "en", {
-          numeric: true,
-          sensitivity: "base",
-        });
+      ? valueA.localeCompare(valueB, "en")
+      : valueB.localeCompare(valueA, "en");
   });
+};
+
+const isValidDate = (date: string) => {
+  return !isNaN(Date.parse(date));
 };
